bcbd39c55e53cc3b076d14f64aad18f62659752a
Brokewala <brokewala@gmail.com>
2024-10-02 16:43:29 +0300

    imagers

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import "./styles/Project.scss"
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper/modules';
@@ -6,54 +5,15 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 // import { motion, useScroll } from "framer-motion"
-import project1 from "../assets/Capture d’écran du 2024-08-25 13-58-47.png"
-import project2 from "../assets/Capture d’écran du 2024-08-25 13-59-04.png"
-import project3 from "../assets/Capture d’écran du 2024-08-25 13-59-19.png"
+import project1 from "../assets/project1.png"
+import project2 from "../assets/project2.png"
+import project3 from "../assets/project3.png"
+import project4 from "../assets/project4.png"
+import project5 from "../assets/project5.png"
+import project6 from "../assets/project6.png"
+import project7 from "../assets/project7.png"
+import project8 from "../assets/project8.png"
 
-
-const ProjectData = [
-  {
-    img: "/src/assets/Capture d’écran du 2024-08-25 13-58-47.png",
-    title: "Capture",
-    link: "https://lodphin24.netlify.app/"
-
-  },
-  {
-    img: "/src/assets/Capture d’écran du 2024-08-25 13-59-04.png",
-    title: "Capture",
-    link: "https://lodphin24.netlify.app/"
-  },
-  {
-    img: "/src/assets/Capture d’écran du 2024-08-25 13-59-19.png",
-    title: "Capture",
-    link: "https://lodphin24.netlify.app/"
-  },
-  {
-    img: "/src/assets/Capture d’écran du 2024-08-25 13-58-13.png",
-    title: "Capture",
-    link: "https://lodphin24.netlify.app/"
-  },
-  {
-    img: "/src/assets/Capture d’écran du 2024-08-25 13-57-33.png",
-    title: "Capture",
-    link: "https://lodphin24.netlify.app/"
-  },
-]
-
-const Cards = ({ img, title, link }) => (
-  <div className='project-cards'>
-    <div className="project-card-img">
-      <img src={img} alt="project1" />
-    </div>
-    <div className="project-card-content">
-      <h4>{title}</h4>
-      <a href={link} target='__blanks' >
-        <button>Discover</button>
-        <img src="/src/assets/icons8-droite-24.png" alt={title} />
-      </a>
-    </div>
-  </div>
-)
 
 export default function Project() {
   return (
@@ -93,14 +53,154 @@ export default function Project() {
               }
             }}
           >
-            {
-              ProjectData.map((data, index) => (
-                <SwiperSlide key={index} >
-                  <Cards img={data.img} title={data.title} link={data.link} />
-                </SwiperSlide>
-              ))
-            }
+            {/* slide1 */}
+            <SwiperSlide >
+              <div className='project-cards'>
+                <div className="project-card-img">
+                  <img src={project1} alt="project1" />
+                </div>
+                <div className="project-card-content">
+                  <h4>My portfolio</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident perferendis modi voluptas itaque unde hic reiciendis suscipit, quod alias nam expedita architecto, natus laboriosam officia debitis sapiente. Blanditiis, illum sint.
+                  </p>
+                  <a href={"https://lodphin24.netlify.app/"} target='__blanks' >
+                    <button>Discover</button>
+                    <img src="/src/assets/icons8-droite-24.png" alt="image" />
+                  </a>
+                </div>
+              </div>
+            </SwiperSlide>
+            {/* slide2 */}
+            <SwiperSlide >
+              <div className='project-cards'>
+                <div className="project-card-img">
+                  <img src={project2} alt="project1" />
+                </div>
+                <div className="project-card-content">
+                  <h4>zafy tody</h4>
+                  <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id accusamus perferendis nemo. Blanditiis minima, iste voluptatem aliquam, iure sunt ducimus tenetur repellat saepe similique possimus, delectus aliquid amet quam mollitia.
+                  </p>
+                  <a href={"https://lodphin24.netlify.app/"} target='__blanks' >
+                    <button>Discover</button>
+                    <img src="/src/assets/icons8-droite-24.png" alt="image" />
+                  </a>
+                </div>
+              </div>
+            </SwiperSlide>
 
+            {/* slide3 */}
+            <SwiperSlide >
+              <div className='project-cards'>
+                <div className="project-card-img">
+                  <img src={project3} alt="project1" />
+                </div>
+                <div className="project-card-content">
+                  <h4>zafy tody</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates obcaecati sint qui, pariatur necessitatibus atque ipsa, accusamus nam quis labore rerum modi quo sapiente accusantium earum nemo dolore rem tenetur.
+                  </p>
+                  <a href={"https://lodphin24.netlify.app/"} target='__blanks' >
+                    <button>Discover</button>
+                    <img src="/src/assets/icons8-droite-24.png" alt="image" />
+                  </a>
+                </div>
+              </div>
+            </SwiperSlide>
+
+            {/* slide4 */}
+            <SwiperSlide >
+              <div className='project-cards'>
+                <div className="project-card-img">
+                  <img src={project4} alt="project1" />
+                </div>
+                <div className="project-card-content">
+                  <h4>zafy tody</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates obcaecati sint qui, pariatur necessitatibus atque ipsa, accusamus nam quis labore rerum modi quo sapiente accusantium earum nemo dolore rem tenetur.
+                  </p>
+                  <a href={"https://lodphin24.netlify.app/"} target='__blanks' >
+                    <button>Discover</button>
+                    <img src="/src/assets/icons8-droite-24.png" alt="image" />
+                  </a>
+                </div>
+              </div>
+            </SwiperSlide>
+
+            {/* slide5 */}
+            <SwiperSlide >
+              <div className='project-cards'>
+                <div className="project-card-img">
+                  <img src={project5} alt="project1" />
+                </div>
+                <div className="project-card-content">
+                  <h4>zafy tody</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates obcaecati sint qui, pariatur necessitatibus atque ipsa, accusamus nam quis labore rerum modi quo sapiente accusantium earum nemo dolore rem tenetur.
+                  </p>
+                  <a href={"https://lodphin24.netlify.app/"} target='__blanks' >
+                    <button>Discover</button>
+                    <img src="/src/assets/icons8-droite-24.png" alt="image" />
+                  </a>
+                </div>
+              </div>
+            </SwiperSlide>
+
+            {/* slide6 */}
+            <SwiperSlide >
+              <div className='project-cards'>
+                <div className="project-card-img">
+                  <img src={project6} alt="project1" />
+                </div>
+                <div className="project-card-content">
+                  <h4>zafy tody</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates obcaecati sint qui, pariatur necessitatibus atque ipsa, accusamus nam quis labore rerum modi quo sapiente accusantium earum nemo dolore rem tenetur.
+                  </p>
+                  <a href={"https://lodphin24.netlify.app/"} target='__blanks' >
+                    <button>Discover</button>
+                    <img src="/src/assets/icons8-droite-24.png" alt="image" />
+                  </a>
+                </div>
+              </div>
+            </SwiperSlide>
+            {/* slide7 */}
+            <SwiperSlide >
+              <div className='project-cards'>
+                <div className="project-card-img">
+                  <img src={project7} alt="project1" />
+                </div>
+                <div className="project-card-content">
+                  <h4>zafy tody</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates obcaecati sint qui, pariatur necessitatibus atque ipsa, accusamus nam quis labore rerum modi quo sapiente accusantium earum nemo dolore rem tenetur.
+                  </p>
+                  <a href={"https://lodphin24.netlify.app/"} target='__blanks' >
+                    <button>Discover</button>
+                    <img src="/src/assets/icons8-droite-24.png" alt="image" />
+                  </a>
+                </div>
+              </div>
+            </SwiperSlide>
+            {/* slide8 */}
+            <SwiperSlide >
+              <div className='project-cards'>
+                <div className="project-card-img">
+                  <img src={project8} alt="project1" />
+                </div>
+                <div className="project-card-content">
+                  <h4>zafy tody</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates obcaecati sint qui, pariatur necessitatibus atque ipsa, accusamus nam quis labore rerum modi quo sapiente accusantium earum nemo dolore rem tenetur.
+                  </p>
+                  <a href={"https://lodphin24.netlify.app/"} target='__blanks' >
+                    <button>Discover</button>
+                    <img src="/src/assets/icons8-droite-24.png" alt="image" />
+                  </a>
+                </div>
+              </div>
+            </SwiperSlide>
           </Swiper>
         </div>
       </div>
